a1f0cef9f9be5131c049e37ef62d974b95ef58f8
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-10 23:25:41 +0300

    Setting PostgreSQL as database

--- learningspace/settings.py
+++ learningspace/settings.py
@@ -81,7 +81,14 @@ WSGI_APPLICATION = 'learningspace.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 }
 
 
