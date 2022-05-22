a43a162d2e90f6c3f074ed632b270d1253710cdf
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-23 02:23:14 +0300

    Learning space basic models

--- space/models.py
+++ space/models.py
@@ -3,7 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Space(models.Model):
-    name = models.CharField(max_length= 32 , null = False)
+    name = models.CharField(max_length= 32 , null = False, unique=True)
     description = models.CharField(max_length= 280 , null = True)
     date = models.DateTimeField(auto_now_add= True)
-    spaceImage = models.ImageField(upload_to = "LearningSpaceImages")
+    spaceImage = models.ImageField(upload_to = "space/LearningSpaceImages")
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    description = models.CharField(max_length= 32, null=False)
+    space = models.ForeignKey(Space, null= False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+class LearningMaterialVideos(models.Model):
+    title = models.CharField(max_length= 128, null=False)
+    space = models.ForeignKey(Space, null= False, on_delete=models.CASCADE)
+    sequence = models.IntegerField(null = False)
+
+    def __str__(self):
+        return self.title
