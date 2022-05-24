b2e25be9ca1ce62905be763a7b9db0b04f7fc9c9
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-25 00:54:46 +0300

    forum application created

---
+++ forum/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from forum.models import Post, Answer
+
+class Post_form(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("title","text")
+
+class Answer_form(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ("text")
---
+++ forum/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+from django.forms import modelformset_factory
+from django.utils import timezone
+from space.models import Space
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateField(default=timezone.now)
+    published_date = models.DateField(blank=True,null=True)
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    def __str__(self) :
+        return self.title
+
+# Create your models here.
+
+class Answer(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    published_date = models.DateField(default=timezone.now)
