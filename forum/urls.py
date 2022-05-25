0f2f850031fbe0db43154e50e8145f68a6dfe08c
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-26 01:59:00 +0300

    Forum application urls are created

--- forum/urls.py
+++ forum/urls.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
+urlpatterns = [
+    path('space/<int:space_id>/forum/', views.list_forum , name= 'list_forum'),
+    path('space/<int:space_id>/forum/create_post', views.post_forum , name= 'post_forum'),
+    path('space/<int:space_id>/forum/<int:post_id>', views.post_detail , name= 'post_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
