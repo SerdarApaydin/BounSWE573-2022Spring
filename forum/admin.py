4a9251a20d56bb6ea2b6719fecff420a9ec8c655
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-26 02:01:20 +0300

    Forum models are added to admin page

--- forum/admin.py
+++ forum/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from forum.models import Post, Answer
+# Register your models here.
 
+class AnswerAdmin(admin.TabularInline):
+    model = Answer
+
+class PostAdmin(admin.TabularInline):
+    model = Post
+    inlines = [AnswerAdmin]
--- space/admin.py
+++ space/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from space.models import Space, Tag, LearningMaterials
+from forum.admin import PostAdmin,AnswerAdmin
 # Register your models here.
 
 class TagAdmin(admin.TabularInline):
@@ -9,7 +10,7 @@ class LearningMaterialsAdmin(admin.StackedInline):
     model = LearningMaterials
 
 class SpaceAdmin(admin.ModelAdmin):
-    inlines = [TagAdmin, LearningMaterialsAdmin]
+    inlines = [TagAdmin, LearningMaterialsAdmin, PostAdmin]
 
 
 admin.site.register(Space, SpaceAdmin)
