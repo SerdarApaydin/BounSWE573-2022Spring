faba084fb93dfae3d56f63110a8d637e9c135dfa
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-29 20:58:10 +0300

    Quiz application urls are defined

---
+++ quiz/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('space/<int:space_id>/quizes/', views.list_quizes , name= 'list_quizes'),
+    path('space/<int:space_id>/quizes/create_quiz', views.create_quiz , name= 'create_quiz'),
+    path('space/<int:space_id>/quizes/<int:quiz_id>', views.quiz_detail , name= 'quiz_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
