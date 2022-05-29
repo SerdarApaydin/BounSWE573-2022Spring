4c9e0091ef191aaa013a5cb0a1b5ecf16ee5e23d
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-29 16:53:34 +0300

    Forum application view function update

--- forum/views.py
+++ forum/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from forum.models import Post, Answer
 from forum.forms import Post_form,Answer_form
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-import space
 from space.models import Space
 
 @login_required
 def list_forum(request,space_id):
     space = Space.objects.get(id = space_id)
     posts = Post.objects.filter(space = space).order_by('published_date')
-    context = {"posts" : posts}
+    context = {"posts" : posts , "space" : space}
     return render(request, template_name="forum/forum_page.html" , context = context)
 
 @login_required
@@ -18,12 +17,12 @@ def post_answer(request,space_id,post_id):
     space = Space.objects.get(id = space_id)
     post = Post.objects.get( id = post_id)
     answer = Answer.objects.filter(post = post).order_by("published_date")
-    form = Answer_form()
+    answer_form = Answer_form()
     context = {
         "space" : space,
         "post"  : post,
         "answer": answer,
-        "form"  : form,
+        "answer_form"  : answer_form,
     }
     return render(request,'forum/post_detail.html', context = context)
 
@@ -37,7 +36,9 @@ def post_forum(request, space_id):
             post.author = request.user
             post.space = space
             post.save()
-            return render(request, "forum/forum_page.html")
+            answers = Answer.objects.filter(post = post).order_by("published_date")
+            answer_form = Answer_form(data=request.POST)
+            return render(request, "forum/post_detail.html", {"space" : space , "post":post, "answers" : answers, "answer_form" : answer_form})
     else:
         form = Post_form()
     return render(request,'forum/create_post.html',{"form":form , "space" : space})
@@ -47,12 +48,15 @@ def post_detail(request,space_id,post_id):
     space = Space.objects.get(id = space_id)
     post = Post.objects.get( id = post_id)
     if request.method == "POST":
-        form = Answer_form(data=request.POST)
-        if form.is_valid():
-            form.save()
+        answer_form = Answer_form(data=request.POST)
+        if answer_form.is_valid():
+            answer = answer_form.save(commit=False)
+            answer.author = request.user
+            answer.post = post
+            answer.save()
             answers = Answer.objects.filter(post = post).order_by("published_date")
-            return render(request, "forum/post_detail.html",{"form":form , "space" : space , "post" : post ,"answers" : answers})
+            return render(request, "forum/post_detail.html",{"answer_form":answer_form , "space" : space , "post" : post ,"answers" : answers})
     else:
-        form = Answer_form()
+        answer_form = Answer_form()
         answers = Answer.objects.filter(post = post).order_by("published_date")
-    return render(request, "forum/post_detail.html",{"form":form , "space" : space , "post" : post ,"answers" : answers})
+    return render(request, "forum/post_detail.html",{"answer_form":answer_form , "space" : space , "post" : post ,"answers" : answers})
