21760858f06ba770bdb3d40aa2d511ad9131c46c
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-31 09:01:08 +0300

    Quiz url pages are updated

--- quiz/urls.py
+++ quiz/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('space/<int:space_id>/quizes/', views.list_quizes , name= 'list_quizes'),
     path('space/<int:space_id>/quizes/create_quiz', views.create_quiz , name= 'create_quiz'),
     path('space/<int:space_id>/quizes/<int:quiz_id>', views.quiz_detail , name= 'quiz_detail'),
+    path('space/<int:space_id>/quizes/<int:quiz_id>/create_question', views.create_question , name= 'create_question'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
