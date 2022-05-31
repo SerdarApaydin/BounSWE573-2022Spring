eb170fa7bcab85f271b44758e81176cc5225bb21
SerdarApaydin <100719207+SerdarApaydin@users.noreply.github.com>
2022-05-31 09:00:34 +0300

    Quiz models update

--- quiz/models.py
+++ quiz/models.py
@@ -9,11 +9,12 @@ class Quiz(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=140)
     description = models.CharField(max_length=500)    
-    number_of_questions = models.IntegerField(default=1)
     published_date = models.DateField(default=timezone.now)
     
     def __str__(self):
         return self.title
+
+true_choices = [("option1" , "option1"),("option2" ,"option2"),("option3" , "option3"),("option4", "option4"),]
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
@@ -22,9 +23,7 @@ class Question(models.Model):
     option2 = models.CharField(max_length=280,null=True)
     option3 = models.CharField(max_length=280,null=True)
     option4 = models.CharField(max_length=280,null=True)
-    true_answer = models.CharField(max_length=280,null=True)
+    true_answer = models.CharField(max_length=280, choices=true_choices, default= option1, null=True, blank=True)
 
     def __str__(self):
         return self.question
-
-
